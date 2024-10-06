e4e8a1bea131e95ae39c22e2734b0d918c364ca9
Harshaan Dhillon <harshaandsxf@gmail.com>
2024-10-06 19:05:11 +0100

    Added node code to oringal

---
+++ server.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
+const PORT = process.env.PORT || 3002;
+const path = require('path');  // Make sure to require 'path'
+
+// Airtable API details
+const airtableApiUrlMenuTable = `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/${process.env.AIRTABLE_MENU_NAME}`;
+const airtableApiUrlReviewsTable = `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/${process.env.AIRTABLE_REVIEWS_NAME}`;
+const airtableToken = process.env.AIRTABLE_API_KEY;
+
+// Enable CORS (for testing)
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+app.use(express.static(path.join(__dirname, 'public')));
+// Route to fetch menu items from Airtable
+app.get('/api/menu', async (req, res) => {
+    try {
+        const response = await fetch(airtableApiUrlMenuTable, {
+            headers: {
+                Authorization: `Bearer ${airtableToken}`
+            }
+        });
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching data from Airtable' });
+    }
+});
+
+// Route to fetch menu items from Airtable
+app.get('/api/reviews', async (req, res) => {
+    try {
+        const response = await fetch(airtableApiUrlReviewsTable, {
+            headers: {
+                Authorization: `Bearer ${airtableToken}`
+            }
+        });
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching data from Airtable' });
+    }
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
